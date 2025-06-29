2e00cda29d03c61abcc3888bc0f9e08f2fbbbb4c
Cincidial <cincidialstudios@gmail.com>
2025-06-29 13:52:51 -0600

    Horizontal Pokemon card

--- src/components/TribeCapsule.tsx
+++ src/components/TribeCapsule.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export default function TribeCapsule({ tribe }: { tribe: Tribe }): ReactNode {
     return (
         <span
-            className="rounded-full w-fit h-fit px-2 py-1 text-white text-shadow-xs/100 text-s font-semibold cursor-default bg-amber-700"
+            className="rounded-full w-fit h-fit px-2 py-1 text-white text-shadow-xs/100 font-semibold cursor-default bg-amber-700"
             title={tribe.description}
         >
             {tribe.name}
