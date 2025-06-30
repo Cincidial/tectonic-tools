f20518ed12905f8df915fc579167cd252b46e3eb
Cincidial <cincidialstudios@gmail.com>
2025-06-30 16:32:51 -0600

    Moved playthrough classes into their own file

--- src/app/tracker/page.tsx
+++ src/app/tracker/page.tsx
@@ -7,6 +7,7 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { Fragment, ReactNode, useEffect, useState } from "react";
+import { EncounterPick, Playthrough } from "../data/playthrough";
 import { Item } from "../data/tectonic/Item";
 import { Pokemon } from "../data/tectonic/Pokemon";
 import { TectonicData } from "../data/tectonic/TectonicData";
@@ -28,130 +29,6 @@ const tableDisplayNameMap: Record<string, string> = {
     ActiveWater: "Deep Water",
     FishingContest: "Surfing",
 };
-
-class EncounterPick {
-    encounterMonId: string;
-    monId: string;
-
-    constructor(encounterMonId: string, monId: string) {
-        this.encounterMonId = encounterMonId;
-        this.monId = monId;
-    }
-}
-
-class LocationPickData {
-    picks: EncounterPick[] = [];
-    flagMissing: boolean = false;
-}
-
-class Playthrough {
-    private static localStorageKey = "EncounterTrackerLocalStorageKey_V1";
-    private static localData: Playthrough[] = [];
-
-    private key: number;
-    private name: string = "Playthrough 1";
-    private locationPickData: Record<string, LocationPickData> = {};
-
-    private constructor(name: string) {
-        this.key = performance.now();
-        this.name = name;
-    }
-
-    static addNewPlaythrough(name?: string): number {
-        const playthrough = new Playthrough(name ?? "New Playthrough");
-        this.localData.push(playthrough);
-
-        this.saveLocalData();
-        return playthrough.key;
-    }
-
-    static getPlayThroughs(): number[] {
-        return this.localData.map((x) => x.key);
-    }
-
-    static getPlayThrough(key?: number): Playthrough | undefined {
-        return this.localData.find((x) => x.key == key);
-    }
-
-    static loadLocalData(): Playthrough[] {
-        const value = localStorage.getItem(this.localStorageKey);
-        const loaded = (this.localData = value ? JSON.parse(value) : []) as Playthrough[];
-
-        this.localData = loaded.map((x) => {
-            const playthrough = new Playthrough(x.name);
-            playthrough.key = x.key;
-            playthrough.locationPickData = x.locationPickData;
-
-            return playthrough;
-        });
-        return this.localData;
-    }
-
-    static saveLocalData() {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.localData));
-    }
-
-    setName(name: string) {
-        this.name = name;
-        Playthrough.saveLocalData();
-    }
-
-    getName(): string {
-        return this.name;
-    }
-
-    addPick(key: string, pick: EncounterPick) {
-        if (!(key in this.locationPickData)) {
-            this.locationPickData[key] = new LocationPickData();
-        }
-
-        this.locationPickData[key].picks.push(pick);
-        Playthrough.saveLocalData();
-    }
-
-    removePick(key: string, encounterMonId: string) {
-        this.locationPickData[key].picks = this.locationPickData[key].picks.filter(
-            (x) => x.encounterMonId != encounterMonId
-        );
-        Playthrough.saveLocalData();
-    }
-
-    hasPick(key: string, encounterMonId: string): boolean {
-        if (key in this.locationPickData) {
-            return this.locationPickData[key].picks.some((x) => x.encounterMonId == encounterMonId);
-        }
-
-        return false;
-    }
-
-    hasPickAnywhere(encounterMonId: string): boolean {
-        return Object.values(this.locationPickData).some(
-            (x) => !x.flagMissing && x.picks.some((x) => x.encounterMonId == encounterMonId)
-        );
-    }
-
-    setPickMissed(key: string, missed: boolean): void {
-        if (!(key in this.locationPickData)) {
-            this.locationPickData[key] = new LocationPickData();
-        }
-
-        this.locationPickData[key].flagMissing = missed;
-        Playthrough.saveLocalData();
-    }
-
-    wasPickMissed(key: string): boolean {
-        if (key in this.locationPickData) {
-            return this.locationPickData[key].flagMissing;
-        }
-
-        return false;
-    }
-
-    delete() {
-        Playthrough.localData = Playthrough.localData.filter((x) => x.key != this.key);
-        Playthrough.saveLocalData();
-    }
-}
 
 class EncounterDisplayData {
     key: string;
