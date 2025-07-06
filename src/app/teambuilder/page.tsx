083a2a76949b3faa1c155c0f93b484e0544593fe
Cincidial <cincidialstudios@gmail.com>
2025-07-06 12:39:50 -0600

    Usage of team manager on builder page

--- src/app/teambuilder/page.tsx
+++ src/app/teambuilder/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import BasicButton from "@/components/BasicButton";
 import { getTypeColorClass } from "@/components/colours";
 import { MiniDexFilter } from "@/components/MiniDexFilter";
 import PageHeader, { PageType } from "@/components/PageHeader";
 import PokemonCardHorizontal from "@/components/PokemonCardHorizontal";
+import SavedTeamManager from "@/components/SavedTeamManager";
 import TribeCapsule from "@/components/TribeCapsule";
 import TypeBadge, { TypeBadgeElementEnum } from "@/components/TypeBadge";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import TypeChartCell from "../../components/TypeChartCell";
-import { decodeTeam, encodeTeam } from "../data/teamExport";
 import { Pokemon } from "../data/tectonic/Pokemon";
 import { TectonicData } from "../data/tectonic/TectonicData";
 import { calcBestMoveMatchup, calcTypeMatchup } from "../data/typeChart";
@@ -21,24 +20,7 @@ import DefTotalCell, { CompareEnum } from "./components/DefTotalCell";
 import MatchupMonCell from "./components/MatchupMonCell";
 
 const TeamBuilder: NextPage = () => {
-    const [teamCode, setTeamCode] = useState<string>("");
     const [party, setParty] = useState<PartyPokemon[]>([]);
-
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            const params = new URLSearchParams(window.location.search);
-            const teamParam = params.get("team");
-            if (teamParam) {
-                setTeamCode(teamParam);
-                try {
-                    setParty(decodeTeam(teamParam));
-                } catch (error) {
-                    console.error("Import error:", error);
-                    alert("Invalid team code! Please check and try again.");
-                }
-            }
-        }
-    }, []);
 
     const tribeCounts = Object.fromEntries(Object.values(TectonicData.tribes).map((t) => [t.id, 0]));
     for (const partyMon of party) {
@@ -46,23 +28,6 @@ const TeamBuilder: NextPage = () => {
             ? Object.keys(TectonicData.tribes)
             : partyMon.species.tribes.map((t) => t.id)) {
             tribeCounts[tribe]++;
-        }
-    }
-
-    function exportTeam() {
-        const code = encodeTeam(party);
-        setTeamCode(code);
-        navigator.clipboard.writeText(code);
-        alert(`Team copied to clipboard!`);
-    }
-
-    function importTeam() {
-        try {
-            setParty(decodeTeam(teamCode).filter((x) => x.species != Pokemon.NULL));
-            alert("Team imported successfully!");
-        } catch (error) {
-            console.error("Import error:", error);
-            alert("Invalid team code! Please check and try again.");
         }
     }
 
@@ -82,21 +47,10 @@ const TeamBuilder: NextPage = () => {
 
             <main className="container mx-auto">
                 <div className="flex flex-col justify-center items-center pb-10 relative">
-                    <div className="flex gap-2 mt-3">
-                        <input
-                            type="text"
-                            placeholder="Team code"
-                            className="px-4 py-2 border border-gray-300 rounded-md"
-                            value={teamCode}
-                            onChange={(e) => setTeamCode(e.target.value)}
-                        />
-                        <BasicButton onClick={importTeam}>Import</BasicButton>
-                        <BasicButton onClick={exportTeam}>Export</BasicButton>
-                    </div>
                     <MiniDexFilter onMon={addPokemon} />
 
                     <hr className="my-3 w-full text-blue-500/50" />
-                    <div className="flex flex-wrap gap-2 mt-2 p-2">
+                    <div className="flex flex-wrap gap-2">
                         {Object.keys(tribeCounts)
                             .filter((t) => tribeCounts[t] >= 1)
                             .sort((a, b) => tribeCounts[b] - tribeCounts[a])
@@ -104,7 +58,7 @@ const TeamBuilder: NextPage = () => {
                                 <TribeCapsule key={t} tribe={TectonicData.tribes[t]} count={tribeCounts[t]} />
                             ))}
                     </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 mx-auto mt-2">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 mx-auto my-1">
                         {party.map((x, index) => (
                             <PokemonCardHorizontal
                                 key={index}
@@ -114,6 +68,7 @@ const TeamBuilder: NextPage = () => {
                             />
                         ))}
                     </div>
+                    <SavedTeamManager onLoad={setParty} exportMons={party} />
 
                     <hr className="my-3 w-full text-blue-500/50" />
                     <div className="flex flex-wrap justify-center gap-2">
