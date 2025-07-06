62b4676329ed4de625e24f60412440194bfefe97
Cincidial <cincidialstudios@gmail.com>
2025-07-06 15:36:31 -0600

    Revert css change

--- src/app/pokedex/page.tsx
+++ src/app/pokedex/page.tsx
@@ -359,7 +359,7 @@ const Home: NextPage = () => {
                     </table>
                 </TabContent>
                 <TabContent tab="Items" activeTab={activeTab}>
-                    <div className="overflow-x-auto">
+                    <div className="overflow-x-auto mx-auto">
                         <table>
                             <thead className="bg-gray-800">
                                 <tr>
